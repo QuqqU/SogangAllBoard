5202d8973776729ade4a8fafaf676a2ececfbb86
QuqqU <rldnd913@naver.com>
2018-05-20 16:07:50 +0900

    Initial Commit

---
+++ SogangAllBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SogangAllBoard
+//
+//  Created by 정기웅 on 2018. 5. 20..
+//  Copyright © 2018년 정기웅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
